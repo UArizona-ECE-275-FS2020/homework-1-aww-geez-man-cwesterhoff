f0bab9a021600e018ca4bdee5da7c917408509fe
cwesterhoff <70231243+cwesterhoff@users.noreply.github.com>
2020-09-24 12:48:46 -0700

    Submitting homework 1

---
+++ Morty.cpp
@@ -0,0 +1,58 @@
+/*
+ __  __            _
+|  \/  | ___  _ __| |_ _   _        ___ _ __  _ __
+| |\/| |/ _ \| '__| __| | | |      / __| '_ \| '_ \
+| |  | | (_) | |  | |_| |_| |  _  | (__| |_) | |_) |
+|_|  |_|\___/|_|   \__|\__, | (_)  \___| .__/| .__/
+                       |___/           |_|   |_|
+*/
+
+// Include the Morty header file
+#include "Morty.hpp"
+
+void MortyC137(int start, int stop, int step)
+{
+    std::cout << "Morty C137 says: " << std::endl;
+
+
+    for (start; start <= stop; start += step) {
+        std::cout << start << ":";
+        
+            if ((start % 3) == 0) {
+                std::cout << " Aww";
+            }
+
+            if ((start % 5) == 0) {
+                std::cout << " Geez";
+            }
+
+            if ((start % 15) == 0) {
+                std::cout << " Man";
+            }
+        std::cout << std::endl;
+
+    }
+}
+
+void MortyZ286(int start, int stop, int step)
+{
+    std::cout << "Morty Z286 says: " << std::endl;
+
+    for (start; start <= stop; start += step) {
+        std::cout << start << ":";
+
+        if ((start % 15) == 0) {
+            std::cout << " naM";
+        }
+
+        if ((start % 5) == 0) {
+            std::cout << " zeeG";
+        }
+
+        if ((start % 3) == 0) {
+            std::cout << " wwA";
+        }
+        std::cout << std::endl;
+
+    }
+}
